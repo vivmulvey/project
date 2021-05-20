e5ff61b0926ad761d6a7955aad20351479eef314
Eoan O Dea <odeaeoan@gmail.com>
2021-05-20 14:12:49 +0200

    Added expense page

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:text_recognition_project/screens/view_expenses_screen.dart';
 
 class Details extends StatefulWidget {
   final String text;
@@ -43,7 +44,9 @@ class _DetailsState extends State<Details> {
                           "vendor": widget.parsedData["Vendor"],
                           "total": widget.parsedData["Total"],
                           "email": _auth.currentUser.email
-                        })
+                        }).then((value) => Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => ViewExpenses())))
                       })
               // SelectableText(
               //     widget.text.isEmpty ? 'No Text Available' : widget.text),
--- lib/screens/view_expenses_screen.dart
+++ lib/screens/view_expenses_screen.dart
@@ -1,26 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-// class ViewExpenses extends StatefulWidget {
-//   final String text;
-//   final Map parsedData;
-//   Details(this.text, this.parsedData);
+class ViewExpenses extends StatefulWidget {
+  ViewExpenses();
 
-//   @override
-//   _ViewExpensesState createState() => _ViewExpensesState();
-// }
+  @override
+  _ViewExpensesState createState() => _ViewExpensesState();
+}
 
-// class _ViewExpensesState extends State<ViewExpenses> {
-//   final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       key: _key,
-//       appBar: AppBar(
-//         backgroundColor: Colors.indigo[900],
-//         title: Text('Expenses'),
-//       ),
-//       body: Container(
-//        ),)
-//     );
-//   }
-// }
+class _ViewExpensesState extends State<ViewExpenses> {
+  final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
+  // final _firestore = FirebaseFirestore.instance;
+  // var snapshot = FirebaseFirestore.instance.collection("expenses").get();
+// snapshot.docs
+  // var list = querySnapshot.documents;
+// var items = _firestore.collection('expenses').get()
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference expenses =
+        FirebaseFirestore.instance.collection('expenses');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: expenses.doc().get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("${data['vendor']} - ${data['price']}");
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+
+  //   return Scaffold(
+  //       key: _key,
+  //       appBar: AppBar(
+  //         backgroundColor: Colors.indigo[900],
+  //         title: Text('Expenses'),
+  //       ),
+  //       body: snapshot.docs.length > 0 ? ListView(children: [
+  //         for (var item in snapshot.docs) {
+  //           ListTile(title: "${item['vendor']} - ${item['price']}", subtitle: item['email'],)
+  //         }
+  //       ],)
+  // }
+
+  // getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
+  //   return snapshot.data.documents
+  //       .map((doc) => new ListTile(
+  //           title: new Text(doc["vendor"] + " - " + doc["total"]),
+  //           subtitle: new Text(doc["email"])))
+  //       .toList();
+  // }
+}
